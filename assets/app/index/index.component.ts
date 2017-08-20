7f07cdffada72dd7d75a02499f5f812cc069fa22
Harvey Chui <chuiharvey@gmail.com>
2017-08-20 08:57:56 -0700

    web client is basically done

--- assets/app/index/index.component.ts
+++ assets/app/index/index.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'index-page',
-    templateUrl: './index.component.html'
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.css']
 })
 export class IndexComponent{
 }
